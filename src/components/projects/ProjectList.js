a5622ef177747f975d85b9af7b28dd3ad10da87d
anthony62490 <anthonyjmagno@gmail.com>
2019-05-18 23:43:42 -0500

    linked project content to pages

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ProjectSummary from './ProjectSummary';
+import { Link } from 'react-router-dom'
 
 const ProjectList = ({projects}) => {
   return (
     <div className="project-list section">
       {projects && projects.map(p => {
         return(
-          <ProjectSummary project={p} key={p.id}/>
+          <Link to={`/project/${p.id}`}>
+            <ProjectSummary project={p} key={p.id}/>
+          </Link>
         )
       })}
     </div>
