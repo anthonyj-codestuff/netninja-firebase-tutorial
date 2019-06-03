78df6217570b2236c7f605861312dd2a2a7c90b1
anthony62490 <anthonyjmagno@gmail.com>
2019-06-03 00:37:43 -0500

    auth functionality wip

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,7 +1,17 @@
-const initState = {};
+const initState = {
+  authError: null
+};
 
 const authReducer = (state = initState, action) => {
-  return state;
+  switch(action.type){
+    case 'LOGIN_ERROR':
+      return {...state, authError: action.payload}
+    case 'LOGIN_SUCCESS':
+      console.log("Login success")
+      return {...state, authError: null}
+    default:
+      return state;
+  }
 }
 
 export default authReducer;
